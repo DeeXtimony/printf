44bf2ab1fcc402974ac61bfdb642c981434f3809
DeeXtimony <danieldamilaredaniels@gamil.com>
2023-04-01 01:34:00 -0700

    et_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,41 +1,43 @@
-#include "main.h"
-
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @i: Pointer to the current index in the format string.
+ * @list: List of arguments.
  *
- * Return: Precision.
+ * Returns: The precision value.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+        int curr_i = *i + 1;
+        int precision = -1;
 
-	if (format[curr_i] != '.')
-		return (precision);
+        // Check if the next character is a period.
+        switch (format[curr_i]) {
+                case '.':
+                        precision = 0;
+                        break;
+                default:
+                        return precision;
+        }
 
-	precision = 0;
+        // Parse the precision value.
+        while (format[++curr_i] != '\0') {
+                switch (format[curr_i]) {
+                        case '*':
+                                curr_i++;
+                                precision = va_arg(list, int);
+                                goto end_loop;
+                        default:
+                                if (is_digit(format[curr_i])) {
+                                        precision *= 10;
+                                        precision += format[curr_i] - '0';
+                                } else {
+                                        goto end_loop;
+                                }
+                }
+        }
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (precision);
+        end_loop:
+        *i = curr_i - 1;
+        return precision;
 }
