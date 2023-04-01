fef278bcc7119a57ff6f112c0ea49bc087ba3d2b
DeeXtimony <danieldamilaredaniels@gamil.com>
2023-04-01 01:36:34 -0700

    get_width.c

--- get_width.c
+++ get_width.c
@@ -1,36 +1,39 @@
-#include "main.h"
-
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: Pointer to the current index in the format string.
+ * @list: List of arguments.
  *
- * Return: width.
+ * Returns: The precision value.
  */
-int get_width(const char *format, int *i, va_list list)
+int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i;
-	int width = 0;
+        int curr_i = *i + 1;
+        int precision = -1;
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			width *= 10;
-			width += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			width = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
+        // Check if the next character is a period.
+        if (format[curr_i] == '.') {
+                precision = 0;
 
-	*i = curr_i - 1;
+                // Parse the precision value.
+                while (format[++curr_i] != '\0') {
+                        switch (format[curr_i]) {
+                                case '*':
+                                        curr_i++;
+                                        precision = va_arg(list, int);
+                                        goto done;
+                                default:
+                                        if (is_digit(format[curr_i])) {
+                                                precision *= 10;
+                                                precision += format[curr_i] - '0';
+                                        } else {
+                                                goto done;
+                                        }
+                        }
+                }
+        }
 
-	return (width);
+        done:
+        *i = curr_i - 1;
+        return precision;
 }
